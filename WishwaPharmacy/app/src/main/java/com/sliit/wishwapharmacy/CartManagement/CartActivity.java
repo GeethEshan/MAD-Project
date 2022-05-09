a68864cd7d8a4fc745db413d4203d04a6d1d0df5
geeth eshan <98637377+GeethEshan@users.noreply.github.com>
2022-05-09 08:05:09 +0530

    Add files via upload

---
+++ WishwaPharmacy/app/src/main/java/com/sliit/wishwapharmacy/CartManagement/CartActivity.java
@@ -0,0 +1,192 @@
+package com.sliit.wishwapharmacy.CartManagement;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sliit.wishwapharmacy.AppointmentManagement.Channeling;
+import com.sliit.wishwapharmacy.Homepage;
+import com.sliit.wishwapharmacy.PharmacyManagement.Pharmacy;
+import com.sliit.wishwapharmacy.R;
+import com.sliit.wishwapharmacy.UserManagement.Patient.PatientProfile;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CartActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button btn_checkout;
+    private Map<String, Double> itemKeys = new HashMap<>();
+    String userID;
+    double allTotal;
+    FirebaseAuth firebaseAuth;
+
+    public static final String EXTRA_ITEM_MAP  = "com.sliit.wishwapharmacy.CartManagement.ITEM_MAP";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+        firebaseAuth = FirebaseAuth.getInstance();
+        userID = firebaseAuth.getCurrentUser().getUid();
+
+        recyclerView = findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        btn_checkout = findViewById(R.id.btn_checkout);
+
+        loadCart();
+
+        btn_checkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Checkout = new Intent(CartActivity.this, orderConfirmation.class);
+                Checkout.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                Checkout.putExtra(EXTRA_ITEM_MAP, (Serializable) itemKeys);
+                startActivity(Checkout);
+            }
+        });
+
+        //Navigation
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId((R.id.cartpage));
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.homepage:
+                        startActivity(new Intent(getApplicationContext(), Homepage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.pharmacypage:
+                       startActivity(new Intent(getApplicationContext() , Pharmacy.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.channelingpage:
+                        startActivity(new Intent(getApplicationContext() , Channeling.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.cartpage:
+                        return true;
+                    case R.id.profilepage:
+                        startActivity(new Intent(getApplicationContext() , PatientProfile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void loadCart(){
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("CartList");
+        FirebaseRecyclerOptions<Cart> options = new FirebaseRecyclerOptions.Builder<Cart>().setQuery(cartListRef.child(userID),Cart.class).build();
+        FirebaseRecyclerAdapter<Cart, CartViewHolder> adapter = new FirebaseRecyclerAdapter<Cart, CartViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull CartViewHolder holder, @SuppressLint("RecyclerView") int position, @NonNull Cart model)
+            {
+                DatabaseReference productDBRef = FirebaseDatabase.getInstance().getReference().child("Products");
+
+                productDBRef.child(model.getItemKey()).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            //getting data from the database
+                            String image = snapshot.child("image").getValue().toString();
+                            String price = snapshot.child("price").getValue().toString();
+                            String Name = snapshot.child("productName").getValue().toString();
+                            model.setProductName(Name);
+                            model.setPrice(Double.valueOf(price));
+                            model.setImage(image);
+
+                            holder.txtProductQuantity.setText(String.valueOf(model.getQuantity()));
+                            holder.txtProductPrice.setText("LKR " +model.getPrice());
+                            holder.txtProductName.setText(model.getProductName());
+                            Picasso.get().load(model.getImage()).into(holder.product);
+                        }
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+//                delete
+                holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view)
+                    {
+                        cartListRef.child(userID).child(model.getItemKey()).removeValue().addOnCompleteListener(new OnCompleteListener<Void>(){
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task)
+                            {
+                                if(task.isSuccessful()) {
+                                    Toast.makeText(CartActivity.this, "Item Removed successfully", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    Toast.makeText(CartActivity.this, "Item Removed Unsuccessfully", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+//              checkbox
+                holder.checkProduct.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (holder.checkProduct.isChecked()){
+                            double totalPrice = model.getQuantity() * model.getPrice();
+                            itemKeys.put(model.getItemKey(),totalPrice);
+                            allTotal = allTotal+totalPrice;
+                            btn_checkout.setText("Checkout("+String.valueOf(allTotal)+"0)");
+                        }else if (!holder.checkProduct.isChecked()){
+                            itemKeys.remove(model.getItemKey());
+                            double totalPrice = model.getQuantity() * model.getPrice();
+                            itemKeys.put(model.getItemKey(),totalPrice);
+                            allTotal = allTotal-totalPrice;
+                            btn_checkout.setText("Checkout("+String.valueOf(allTotal)+"0)");
+                        }
+                    }
+                });
+            }
+            @NonNull
+            @Override
+            public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+            {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_item_layout, parent,false);
+                CartViewHolder holder = new CartViewHolder(view);
+                return holder;
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+}
