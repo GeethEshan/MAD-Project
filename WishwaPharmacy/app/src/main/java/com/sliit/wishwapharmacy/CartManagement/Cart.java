6df266e9c93c0752be686c3102a542e8fc21eed3
geeth eshan <98637377+GeethEshan@users.noreply.github.com>
2022-05-09 08:01:29 +0530

    Add files via upload

---
+++ WishwaPharmacy/app/src/main/java/com/sliit/wishwapharmacy/CartManagement/Cart.java
@@ -0,0 +1,56 @@
+package com.sliit.wishwapharmacy.CartManagement;
+
+public class Cart {
+
+    private String productName, ItemKey, Image;
+    private int quantity;
+    private double Price;
+
+    public Cart() {
+    }
+
+    public Cart(String itemKey, int quantity) {
+        ItemKey = itemKey;
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getItemKey() {
+        return ItemKey;
+    }
+
+    public void setItemKey(String itemKey) {
+        ItemKey = itemKey;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+}
