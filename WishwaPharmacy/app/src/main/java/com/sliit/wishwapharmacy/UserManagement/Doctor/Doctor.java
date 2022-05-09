db0c7f28f661a725e214ae6c70be645ee25a3aa2
H.K.A.A.S.Subasinghe <100862954+Arsith@users.noreply.github.com>
2022-05-09 09:00:57 +0530

    User management

---
+++ WishwaPharmacy/app/src/main/java/com/sliit/wishwapharmacy/UserManagement/Doctor/Doctor.java
@@ -0,0 +1,44 @@
+package com.sliit.wishwapharmacy.UserManagement.Doctor;
+
+import com.sliit.wishwapharmacy.UserManagement.Person;
+
+public class Doctor extends Person {
+    private String speciality;
+    private String workingPlace;
+    private String fee;
+
+    public Doctor() {
+    }
+
+    public Doctor(String userID, String username, String email, String phone, String dob, int age, String gender, String password, String image, String speciality,
+                  String workingPlace, String fee) {
+        super(userID, username, email, phone, dob, age, gender, password, image);
+        this.speciality = speciality;
+        this.workingPlace = workingPlace;
+        this.fee = fee;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getWorkingPlace() {
+        return workingPlace;
+    }
+
+    public void setWorkingPlace(String workingPlace) {
+        this.workingPlace = workingPlace;
+    }
+
+    public String getFee() {
+        return fee;
+    }
+
+    public void setFee(String fee) {
+        this.fee = fee;
+    }
+}
