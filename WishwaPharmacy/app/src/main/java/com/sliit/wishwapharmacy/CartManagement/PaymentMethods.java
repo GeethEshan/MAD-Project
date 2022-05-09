39d423bb7e374e78dbc2fb1fd292b0de0c721658
geeth eshan <98637377+GeethEshan@users.noreply.github.com>
2022-05-09 08:07:37 +0530

    Add files via upload

---
+++ WishwaPharmacy/app/src/main/java/com/sliit/wishwapharmacy/CartManagement/PaymentMethods.java
@@ -0,0 +1,16 @@
+package com.sliit.wishwapharmacy.CartManagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sliit.wishwapharmacy.R;
+
+public class PaymentMethods extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_methods);
+    }
+}
